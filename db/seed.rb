38986bba7f1402c29e7d13d8f6c566ee8e67f2a9
drammopo <drammopo@gmail.com>
2014-04-04 00:36:23 +0200

    Added more feedback to migration file.

--- db/seed.rb
+++ db/seed.rb
@@ -1,12 +1,12 @@
-# Cleaning Out
-#Page.delete_all
+#Cleaning Out
+Page.delete_all
 
-#page = Page.create(starting_point: true, content: "You wake up on a road. It's foggy and dampy. In your bag is 30 gold pieces and a bacon sandwich. Which do you choose?")
-#Page.create(conclusion: true, parent_id: page.id, content: "Go into the forest")
-#Page.create(conclusion: true, parent_id: page.id, content: "Walk down the roads")
+page = Page.create(starting_point: true, content: "You wake up on a road. It's foggy and dampy. In your bag is 30 gold pieces and a bacon sandwich. Which do you choose?")
+Page.create(conclusion: true, parent_id: page.id, content: "Go into the forest")
+Page.create(conclusion: true, parent_id: page.id, content: "Walk down the roads")
 
-#puts "There are #{Page.count} Page(s) in the database."
+puts "There are #{Page.count} Page(s) in the database."
 
-# Pages.all.each do |page|
-# puts "Your adventures are : #{page.content}"
-# end
+Page.all.each do |page|
+puts "Your adventures are : #{page.content}"
+end
